f11a5cd2c98a170ae3bc3093d8949e1018ed87c4
André Pintor <130668708+Yfy21@users.noreply.github.com>
2024-01-29 16:35:35 -0300

    Add files via upload

---
+++ bubble_sort.py
@@ -0,0 +1,21 @@
+import random
+
+numbers_list = [random.randint(1, 100) for _ in range(10)]
+
+def bubble_sort(numbers_list):
+    for i in range(len(numbers_list)):
+        for j in range(len(numbers_list) - i - 1):
+            if numbers_list[j] > numbers_list[j+1]:
+                (numbers_list[j], numbers_list[j+1]) = (numbers_list[j + 1], numbers_list[j])
+    return numbers_list
+
+def reverse_bubble_sort(numbers_list):
+    for i in range(len(numbers_list)):
+        for j in range(len(numbers_list)-1, i, -1):
+            if numbers_list[j] < numbers_list[j-1]:
+                (numbers_list[j], numbers_list[j - 1]) = numbers_list[j-1], numbers_list[j]
+    return numbers_list
+
+if __name__ == '__main__':
+    print(bubble_sort(numbers_list))
+    # print(reverse_bubble_sort(numbers_list))
---
+++ selection_sort.py
@@ -0,0 +1,30 @@
+import random
+
+numbers_list = [random.randint(1, 100) for _ in range(10)]
+
+
+def selection_sort(numbers_list):
+    for i in range(len(numbers_list)):
+        current_smallest = numbers_list[i]
+        current_smallest_index = i
+        for j in range(i, len(numbers_list)):
+            if numbers_list[j] < current_smallest:
+                current_smallest = numbers_list[j]
+                current_smallest_index = j
+        (numbers_list[i], numbers_list[current_smallest_index]) = numbers_list[current_smallest_index], numbers_list[i]
+    return numbers_list
+
+def reverse_selection_sort(numbers_list):
+    for i in range(len(numbers_list)):
+        current_smallest = numbers_list[0]
+        current_smallest_index = 0
+        for j in range(len(numbers_list)-i):
+            if numbers_list[j] < current_smallest:
+                current_smallest = numbers_list[j]
+                current_smallest_index = j
+        (numbers_list[-i-1], numbers_list[current_smallest_index]) = numbers_list[current_smallest_index], numbers_list[-i-1]
+    return numbers_list
+
+if __name__ == '__main__':
+    print(selection_sort(numbers_list))
+    # print(reverse_selection_sort(numbers_list))
