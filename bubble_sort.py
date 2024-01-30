2824dbe2da0e15565f3899e9d25074980842deea
André Pintor <130668708+Yfy21@users.noreply.github.com>
2024-01-30 15:31:33 -0300

    Update bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -18,7 +18,7 @@ def bubble_sort(numbers_list):
 def reverse_bubble_sort(numbers_list):
     for i in range(len(numbers_list)):
         for j in range(len(numbers_list)-1, i, -1):
-            if numbers_list[j] < numbers_list[j-1]:
+            if numbers_list[j] > numbers_list[j-1]:
                 (numbers_list[j], numbers_list[j - 1]) = numbers_list[j-1], numbers_list[j]
     return numbers_list
 
