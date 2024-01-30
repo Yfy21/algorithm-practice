53a451f6b6fb2cc991a51990550e1f92165a430a
André Pintor <130668708+Yfy21@users.noreply.github.com>
2024-01-30 15:24:54 -0300

    Update bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,6 +1,10 @@
 import random
 
-numbers_list = [random.randint(1, 100) for _ in range(10)]
+
+def generate_random_numbers(start, finish, n):
+    """Generates n random numbers ranging from the 'start' value to the 'finish' value"""
+    numbers_list = [random.randint(start, finish) for _ in range(n)]
+    return numbers_list
 
 
 def bubble_sort(numbers_list):
@@ -20,5 +24,7 @@ def reverse_bubble_sort(numbers_list):
 
 
 if __name__ == '__main__':
+    numbers_list = generate_random_numbers(0, 100, 10)
     print(bubble_sort(numbers_list))
-    # print(reverse_bubble_sort(numbers_list))
+    numbers_list = generate_random_numbers(0, 100, 10)
+    print(reverse_bubble_sort(numbers_list))
